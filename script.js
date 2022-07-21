f78b2d52c424884edd3fe826981f1106296162f5
Arian Weslley <weslleysantoszbd@gmail.com>
2022-07-21 15:55:09 -0300

    Finalização da Base do Projeto

---
+++ script.js
@@ -0,0 +1,29 @@
+const dias = document.getElementById("dias");
+const horas = document.getElementById("horas");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
+const contagem = document.getElementById("contagem");
+
+const estreiaHOTD = new Date("August 21 22 00:00:00");
+
+// Atualização da Contagem
+function atualizarContagem() {
+  const tempoAtual = new Date();
+  const diff = estreiaHOTD - tempoAtual;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Adicionando Valores no DOM
+  dias.innerHTML = d;
+  horas.innerHTML = h < 10 ? "0" + h : h;
+  minutos.innerHTML = m < 10 ? "0" + m : m;
+  segundos.innerHTML = s < 10 ? "0" + s : s;
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+  // Explicações sobre a interrogação
+}
+
+// Atualização a cada milisegundo.
+setInterval(atualizarContagem, 100);
